b1a5483f6fcec8e32e9c2c2819341d9e9d96dc97
rockytang <rocky.tang168@gmail.com>
2015-04-03 14:46:17 -0700

    Add update score

--- watchout.js
+++ watchout.js
@@ -1,2 +1,31 @@
 // start slingin' some d3 here.
 
+var points = 0;
+var highScore = 0;
+var enemies = [{},{},{}];
+
+var Update = function(data){
+  UpdateScore();
+}
+
+setInterval(function(){Update()}, 1000);
+
+var ResetScore = function(){
+  points = 0;
+}
+
+var UpdateScore = function(){
+  points++;
+  highScore = Math.max(highScore, points);
+
+  console.log(points);
+
+  d3.select(".current").select("span").text(points);
+  d3.select(".high").select("span").text(highScore);
+}
+
+var AppendEnemies = function(){
+
+
+
+}
